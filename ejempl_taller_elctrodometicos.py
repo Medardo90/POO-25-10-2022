c51d604d202e0f6c120dce0c72239ac0854df70c
Medardo90 <105029093+Medardo90@users.noreply.github.com>
2022-10-25 10:14:03 -0500

    Add files via upload

---
+++ ejempl_taller_elctrodometicos.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 25 09:33:00 2022
+
+@author: lab
+"""
+#iniciualiza en el parmentro ******+por defecto
+
+# class Electrodomestico:
+#     def _init_(self):
+#         self.color="blanco"
+#         self.consumo= None 
+#         self.precio = 10
+#     def tablaconsumo(self):# hacer con if siempre hacer con self para que sea un metodo si no es una funcion 
+#         self.consumo = "A"
+        
+#                             #if precio basa == 100 :
+                                
+                                
+#    # como llamar a nuestro metodo constructor     
+# class Licuadora(Electrodomestico):
+#      def _init_(self):
+#          Electrodomestico. __init__(self)
+             
+   
+        
+# licuadora=Electrodomestico()
+# print("color licuadora:",licuadora.color)
+# labadora=Electrodomestico()
+# print("color labadora:",labadora.color)
+
+# licuadora.color="azul"
+# print("color licuadora:",licuadora.color)
+
+# # 2do metodo 
+
+# electrodomestico=[] # sirve parta crear mas electyrodomenticos siempre y cuando heredando 
+# for i in range(5):
+#     electrodomestico.append(Electrodomestico())
+# for i in range(len(electrodomestico)):
+#     print("color: "+str(i), electrodomestico[i].color)
+#     electrodomestico[i].color="verde"
+#     print("color: "+str(i), electrodomestico[i].color)
+    
+    #inicializar color aleatorio
+# import random 
+# lista = ["a","b","c"]
+# print (random.choice(lista))
+
+
+
+import numpy is np
+var_random = np.random.randint(1,5)
+print(var_random)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
