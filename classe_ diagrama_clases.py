1800813aef937b26a1d70b479e1fefe9d115464a
Medardo90 <105029093+Medardo90@users.noreply.github.com>
2022-10-26 10:25:04 -0500

    Add files via upload

---
+++ classe_ diagrama_clases.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 26 09:22:19 2022
+
+@author: lab
+"""
+
+#caracteridsticas del POO
+# emcapsulameinto, herencia, agregacuion, composicion 
+
+"""Diagrama de clases 
+1) nombre de la clase
+2 atributos y metodos
+- en clae uml 
+- privado 
++ publico
+# restrigido 
+* asociocion bidirecional
+
+"""investigar acrca de diagramas de clases como hacer diagramas de calses en python"""
